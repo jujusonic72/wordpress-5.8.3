06df23912a2a26237c2db8bd91061c35280442ef
Julien Poirier Morin <43372587+jujusonic72@users.noreply.github.com>
2022-03-02 15:46:58 -0500

    ajout d'une image de couverture pour le theme

--- 2022-4w4/wp-content/themes/exer-2-Poirier-Morin-Julien/header.php
+++ 2022-4w4/wp-content/themes/exer-2-Poirier-Morin-Julien/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 2 | Trottier Jérôme</title>
+    <title>Exercice 2 | Julien Poirier Morin</title>
     <?php wp_head() ?>
 </head>
 
