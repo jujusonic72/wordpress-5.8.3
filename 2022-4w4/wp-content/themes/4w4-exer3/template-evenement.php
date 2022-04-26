f2f418983d555050cbd842c11bc1f4c044eb4941
Julien Poirier Morin <43372587+jujusonic72@users.noreply.github.com>
2022-04-26 17:39:42 -0400

    premier commmit

--- 2022-4w4/wp-content/themes/4w4-exer3/template-evenement.php
+++ 2022-4w4/wp-content/themes/4w4-exer3/template-evenement.php
@@ -8,11 +8,23 @@
 ?>
 
 <main class="site__main">
-     <h1>Évènement</h1>
-     <?php if (have_posts()): while(have_posts()): the_post(); ?>
-     <?php the_title() ?>
-     <?php the_content() ?>
-     <?php endwhile ?>
+
+     <h1>---- template-evenement.php ------</h1>
+
+     <?php if (have_posts()) : the_post(); ?>
+          <?php the_title() ?>
+          <?php
+          $image = get_field('image');
+          if (!empty($image)) : ?>
+               <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
+          <?php endif ?>
+          <h2>L'endroit</h2>
+          <p><?php the_field('endroit') ?></p>
+          <h2>Organisé par : <?php the_field('organisateur') ?></h2>
+          <p>Date de l'evenement : <?php the_field('date') ?></p>
+          <p>Heure de l'evenement : <?php the_field('heure') ?></p>
+          <p>Description de l'evenement : <?php the_field('resume') ?></p>
+          <?php the_content() ?>
      <?php endif ?>
 </main>
 <?php get_footer() ?>
