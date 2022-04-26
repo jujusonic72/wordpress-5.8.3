c4fe776d2d91c809c7e8287e6a9a709793c36d92
Julien Poirier Morin <43372587+jujusonic72@users.noreply.github.com>
2022-04-26 17:59:46 -0400

    php du gabarit cours

--- 2022-4w4/wp-content/themes/4w4-exer3/gabarits/gabarit-cours.php
+++ 2022-4w4/wp-content/themes/4w4-exer3/gabarits/gabarit-cours.php
@@ -1,9 +1,13 @@
 <div class="cours__conteneur">
     <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
-            <?php $categorie = get_the_category(); ?>
+            <?php 
+                $categorie = get_the_category(); 
+                $descCours = preg_replace('/(<img [^>]*>)/', '', get_the_content());
+                $descCours = apply_filters('the_content', $descCours);
+            ?>
 
-            <a href=<?= get_permalink(); ?> class="carte">
+            <article class="carte">
                 <div class="carte__etiquettes">
                     <div class="carte__etiquettes__sigle">
                         <span class="carte__etiquettes__sigle__contenu"><?= le_sigle(get_the_title()); ?></span>
@@ -13,12 +17,20 @@
                     </div>
                 </div>
                 <h3 class="carte__titre">
-                    <?= le_titre_filtre(get_the_title()); ?>
+                    <a href="<?php echo get_permalink(); ?>">
+                        <?= le_titre_filtre(get_the_title()); ?>
+                    </a>
                 </h3>
+                <p class="carte__desc">
+                    <?= wp_trim_words($descCours, 15, '<button class="cours__desc__suite" href="#"> La suite...</button>'); ?>
+                </p>
                 <div class="carte__miniature">
-                    <?php the_post_thumbnail("thumbnail"); ?>
+                    <?php the_post_thumbnail(); ?>
                 </div>
-            </a>
+                <div class="carte__desc__full">
+                    <?= $descCours ?>
+                </div>
+            </article>
             
         <?php endwhile ?>
     <?php endif ?>
