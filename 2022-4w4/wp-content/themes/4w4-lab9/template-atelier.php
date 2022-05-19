f1f5ea250bfe4275eb85cca50539f5ebe8c7cd5a
Julien Poirier Morin <43372587+jujusonic72@users.noreply.github.com>
2022-05-19 14:33:50 -0400

    dernier commit epreuve finale

--- 2022-4w4/wp-content/themes/4w4-lab9/template-atelier.php
+++ 2022-4w4/wp-content/themes/4w4-lab9/template-atelier.php
@@ -17,15 +17,20 @@ get_header();
           if (!empty($image)) : ?>
                <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
           <?php endif ?>
+          <div class="container__atelier"></div>
+          <div class="atelier__desc">
           <p>Description de l'atelier : <?php the_field('resume') ?></p>
+          </div>
           <h2>Organisé par : <?php the_field('animateur') ?></h2>
-          <p>Date du début de l'atelier : <?php the_field('datedebut') ?></p>
-          <p>Date de la fin de l'atelier : <?php the_field('datefin') ?></p>
+          <div class="atelier__dates">
+          <p>Date du début de l'atelier : <?php the_field('datedebut') ?> Date de la fin de l'atelier : <?php the_field('datefin') ?></p>
+          </div>
           <p>Jour de l'atelier : <?php the_field('jour') ?></p>
-          <p>Heure du début de l'atelier : <?php the_field('heuredebut') ?></p>
-          <p>Heure de la fin de l'atelier : <?php the_field('heurefin') ?></p>
-          <p>Durée de l'atelier : <?php the_field('duree') ?></p>
+          <div class="atelier__heures">
+          <p>Heure du début de l'atelier : <?php the_field('heuredebut') ?> Heure de la fin de l'atelier : <?php the_field('heurefin') ?> Durée de l'atelier : <?php the_field('duree') ?></p>
+          </div>
           <p>Local ou se déroulera l’atelier : <?php the_field('local') ?></p>
+          </div>
           <?php the_content() ?>
      <?php endif ?>
 </main>
